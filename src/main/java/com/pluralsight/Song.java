3dd1476c9755e57d152fdf6a6e5df942a9350f52
sgrinley <shamar808@yahoo.com>
2026-05-18 13:43:06 -0700

    feat: Add Instance Variable

--- src/main/java/com/pluralsight/Song.java
+++ src/main/java/com/pluralsight/Song.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class Song {
+
+//    Add Data Fields
+    private String title;
+    private String artist;
+    private String genre;
+    private long streamCount;
+
+
 }
