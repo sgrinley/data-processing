45342bb481c67ec7cc781e78f2eaa994a27df13a
sgrinley <shamar808@yahoo.com>
2026-05-18 13:45:17 -0700

    feat: Generate Constructor

--- src/main/java/com/pluralsight/Song.java
+++ src/main/java/com/pluralsight/Song.java
@@ -2,11 +2,19 @@ package com.pluralsight;
 
 public class Song {
 
-//    Add Data Fields
+    //    Add Data Fields
     private String title;
     private String artist;
     private String genre;
     private long streamCount;
+
+    //    Generate Constructor
+    public Song(String title, String artist, String genre, long streamCount) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.streamCount = streamCount;
+    }
 
 
 }
